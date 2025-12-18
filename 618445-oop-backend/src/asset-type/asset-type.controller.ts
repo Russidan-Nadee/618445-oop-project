914beb81fb19a9a192a8ceeb234df4b2d16b730b
Russidan <russidan.nadee@gmail.com>
2025-12-18 16:38:55 +0700

    feat: complete CRUD for asset type

--- 618445-oop-backend/src/asset-type/asset-type.controller.ts
+++ 618445-oop-backend/src/asset-type/asset-type.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Get, Delete, Patch, Param, Body } from '@nestjs/common';
+
 import { AssetTypeService } from './asset-type.service';
 import { CreateAssetTypeDto } from './dto/create-asset-type.dto';
 
@@ -6,8 +7,36 @@ import { CreateAssetTypeDto } from './dto/create-asset-type.dto';
 export class AssetTypeController {
    constructor(private readonly assetTypeService: AssetTypeService) { }
 
+   // CREATE
    @Post()
    create(@Body() createAssetTypeDto: CreateAssetTypeDto) {
       return this.assetTypeService.create(createAssetTypeDto);
+   }
+
+   // GET ALL
+   @Get()
+   findAll() {
+      return this.assetTypeService.findAll();
+   }
+
+   // GET BY ID
+   @Get(':id')
+   findOne(@Param('id') id: string) {
+      return this.assetTypeService.findOne(Number(id));
+   }
+
+   //UPDATE
+   @Patch(':id')
+   update(
+      @Param('id') id: string,
+      @Body() body: { name: string },
+   ) {
+      return this.assetTypeService.update(Number(id), body.name);
+   }
+
+   // DELETE
+   @Delete(':id')
+   remove(@Param('id') id: string) {
+      return this.assetTypeService.remove(Number(id));
    }
 }
--- 618445-oop-backend/src/asset-type/asset-type.service.ts
+++ 618445-oop-backend/src/asset-type/asset-type.service.ts
@@ -6,11 +6,39 @@ import { CreateAssetTypeDto } from './dto/create-asset-type.dto';
 export class AssetTypeService {
    constructor(private readonly prisma: PrismaService) { }
 
+   // CREATE
    create(createAssetTypeDto: CreateAssetTypeDto) {
       return this.prisma.assetType.create({
          data: {
             name: createAssetTypeDto.name,
          },
+      });
+   }
+
+   // GET ALL
+   findAll() {
+      return this.prisma.assetType.findMany();
+   }
+
+   // GET BY ID
+   findOne(id: number) {
+      return this.prisma.assetType.findUnique({
+         where: { id },
+      });
+   }
+
+   // UPDATE
+   update(id: number, name: string) {
+      return this.prisma.assetType.update({
+         where: { id },
+         data: { name },
+      });
+   }
+
+   // DELETE
+   remove(id: number) {
+      return this.prisma.assetType.delete({
+         where: { id },
       });
    }
 }
